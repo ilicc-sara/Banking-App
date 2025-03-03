cc16c62c69d43c12a14d761f0349109601578f94
Sara <sarailic431@gmail.com>
2025-03-03 16:36:46 +0100

    ...

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
 "use strict";
 import "./style.css";
+
+const signUp = document.querySelector(".sign-up-link");
+
+console.log(signUp);
+
+signUp.addEventListener("click", function (e) {
+  console.log("kliknuto sign up");
+});
