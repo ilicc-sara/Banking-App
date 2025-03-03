174209bb54976573922c2dd01345379ebf14b6cc
Sara <sarailic431@gmail.com>
2025-03-03 19:21:06 +0100

    ...

--- src/main.js
+++ src/main.js
@@ -10,3 +10,16 @@ signUpLink.addEventListener("click", function (e) {
   signUpForm.classList.remove("hidden");
   overlay.classList.remove("hidden");
 });
+
+overlay.addEventListener("click", function (e) {
+  signUpForm.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+
+class Account {
+  constructor() {
+    this.email = "";
+    this.password = "";
+    this.username = "";
+  }
+}
